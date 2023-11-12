216f5589f10f65c5316aba666268dbfe6c9d6b18
jayant <jayanttripathy1985@gmail.com>
2023-11-12 13:16:29 +0530

    Add project files.

---
+++ BlobStorageAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,23 @@
+﻿using BlobStorageAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlobStorageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        AzureBlobService _service;
+        public FileUploadController(AzureBlobService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadBlobs(List<IFormFile> files)
+        {
+            var response = await _service.UploadFiles(files);
+            return Ok(response);
+        }
+    }
+}
---
+++ BlobStorageAPI/Services/AzureBlobService.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace BlobStorageAPI.Services
+{
+    public class AzureBlobService
+    {
+        BlobServiceClient _blobClient;
+        private readonly IConfiguration _configuration;
+        BlobContainerClient _containerClient;
+        public AzureBlobService(IConfiguration Configuration)
+        {  
+            _configuration = Configuration;
+            _blobClient = new BlobServiceClient(_configuration.GetSection("BlobStorgaeInfo:ConnectionStrings").Value.ToString());
+            _containerClient = _blobClient.GetBlobContainerClient(_configuration.GetSection("BlobStorgaeInfo:StorageName").Value.ToString());
+        }
+
+        public async Task<List<Azure.Response<BlobContentInfo>>> UploadFiles(List<IFormFile> files)
+        {
+
+            var azureResponse = new List<Azure.Response<BlobContentInfo>>();
+            foreach (var file in files)
+            {
+                string fileName = file.FileName;
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Position = 0;
+                    var client = await _containerClient.UploadBlobAsync(fileName, memoryStream, default);
+                    azureResponse.Add(client);
+                }
+            };
+
+            return azureResponse;
+        }
+
+        public async Task<List<BlobItem>> GetUploadedBlobs()
+        {
+            var items = new List<BlobItem>();
+            var uploadedFiles = _containerClient.GetBlobsAsync();
+            await foreach (BlobItem file in uploadedFiles)
+            {
+                items.Add(file);
+            }
+
+            return items;
+        }
+    }
+}
+
